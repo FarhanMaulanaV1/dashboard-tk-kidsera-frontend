708bf398cfe61e648a0bcaf04719eab7766709ab
GrantsV1 <renamamiyat1@gmail.com>
2022-11-18 13:36:49 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'kidseradashboard.v1',
+  appName: 'kidseradashboard-v1',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
